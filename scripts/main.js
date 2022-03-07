ecfc65e4eae857d9082840ff4fc4ab893aaf8dd1
Griffin Lamphere <7319002@isd535.org>
2022-03-07 11:00:16 -0600

    added transitions to nature center

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-const accordion = document.getElementsByClassName('NC-Collapse');
-let NCContent = document.getElementsByClassName('NC-Content')
+var acc = document.getElementsByClassName("NC-Collapse");
+var i;
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
-
-if ("NC-Collapse active") {
-    NCContent.style.height = "100px"
+//displays and hides the nature center body based on wether the button is clicked
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
